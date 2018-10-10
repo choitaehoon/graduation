e8fcce8a67b74235746d5e2741dfb7bb767c7b41
Hosick <33890461+Hosick@users.noreply.github.com>
2018-10-10 21:36:53 +0900

    update

--- src/main/java/net/skhu/controller/LoginController.java
+++ src/main/java/net/skhu/controller/LoginController.java
@@ -13,116 +13,99 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
-public class LoginController
-{
-    @Autowired  StudentRepository studentRepository;
-    @Autowired  DepartmentRepository departmentRepository;
-    @Autowired TypeIdentity typeIdentity;
+public class LoginController {
+    @Autowired
+    StudentRepository studentRepository;
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    TypeIdentity typeIdentity;
+
+    @RequestMapping("main")
+    public String login(Model model, User user) {
+        model.addAttribute("member", typeIdentity.distinct(user.getId(), user.getPassword()));
+        return "login/main";
+    }
+
+    @RequestMapping("myInfo")
+    public String myInfo(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
+        return "login/myInfo";
+    }
+
+    /*
+     * 학생 관리
+     */
+    @RequestMapping("administration")
+    public String administration(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
+        model.addAttribute("student", studentRepository.findAll());
+        return "login/administration";
+    }
 
     @RequestMapping("member")
-    public String member(Student student)
-    {
+    public String member(Student student) {
         studentRepository.save(student);
         return "../login.jsp";
     }
 
     @RequestMapping("signUp")
-    public String signUp()
-    {
+    public String signUp() {
         return "login/signUp";
     }
 
-    @RequestMapping("main")
-    public String login(Model model, User user)
-    {
-        model.addAttribute("member",typeIdentity.distinct(user.getId(), user.getPassword()));
-        return "login/main";
-    }
-
-    /*
-    마이페이지
-     */
-    @RequestMapping("myInfo")
-        public String myInfo(Model model, @RequestParam("type") int type, @RequestParam("id") int id)
-        {
-            model.addAttribute("member",typeIdentity.typeSearch(type, id));
-            return "login/myInfo";
-    }
-
-    /*
-    * 학생 관리
-    */
-    @RequestMapping("studentManager")
-    public String studentManager(Model model, @RequestParam("type") int type ,@RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
-        model.addAttribute("student",studentRepository.findAll());
-        return "login/studentManager";
-    }
-    /*
-     * 학생 수정
-     */
-    @RequestMapping("manager_stu_info")
-    public String manager_stu_info(Model model, @RequestParam("type") int type ,@RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
-        return "login/manager_stu_info";
-    }
-
-
-
 
     @RequestMapping("graduationInfo")
-    public String graduationInfo(Model model, @RequestParam("type") int type, @RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
+    public String graduationInfo(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
         return "login/graduationInfo";
     }
 
     @RequestMapping("before18")
-    public String before18(Model model, @RequestParam("type") int type, @RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
+    public String before18(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
         return "login/before18";
     }
 
     @RequestMapping("after18")
-    public String after18(Model model, @RequestParam("type") int type, @RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
+    public String after18(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
         return "login/after18";
     }
 
     @RequestMapping("grdRegister")
-    public String grdRegister(Model model, @RequestParam("type") int type, @RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
+    public String grdRegister(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
         return "login/grdRegister";
     }
+
     @RequestMapping("grdUpdate")
-    public String grdUpdate(Model model, @RequestParam("type") int type, @RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
+    public String grdUpdate(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
         return "login/grdUpdate";
     }
 
     @RequestMapping("majorUpdate")
-    public String majorUpdate(Model model,@RequestParam("type") int type, @RequestParam("id") int id)
-    {
-        model.addAttribute("member",typeIdentity.typeSearch(type, id));
+    public String majorUpdate(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
         return "login/majorUpdate";
     }
 
     @RequestMapping("noticeBoard")
-    public String notice(Model model, Student student)
-    {
-        model.addAttribute("students",studentRepository.findById(student.getId()));
+    public String notice(Model model, Student student) {
+        model.addAttribute("students", studentRepository.findById(student.getId()));
         return "login/noticeBoard";
     }
+
     @RequestMapping("question")
-    public String question(Model model, Student student)
-    {
-        model.addAttribute("students",studentRepository.findById(student.getId()));
+    public String question(Model model, Student student) {
+        model.addAttribute("students", studentRepository.findById(student.getId()));
         return "login/question";
+    }
+
+    @RequestMapping("help")
+    public String help(Model model, @RequestParam("type") int type, @RequestParam("id") int id) {
+        model.addAttribute("member", typeIdentity.typeSearch(type, id));
+        return "login/help";
     }
 }
