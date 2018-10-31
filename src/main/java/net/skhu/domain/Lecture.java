2c9284feb25df185de7c20122433b1164194dd16
Hosick <33890461+Hosick@users.noreply.github.com>
2018-10-31 13:11:16 +0900

    수업 dto추가

---
+++ src/main/java/net/skhu/domain/Lecture.java
@@ -0,0 +1,82 @@
+package net.skhu.domain;
+
+import lombok.Data;
+
+@Data
+public class Lecture
+{
+    int year;
+    String semester;
+    String id;
+    int split;
+    String title;
+    int admin_id;
+    int credit;
+    String subType;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSplit() {
+        return split;
+    }
+
+    public void setSplit(int split) {
+        this.split = split;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(int admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getSubType() {
+        return subType;
+    }
+
+    public void setSubType(String subType) {
+        this.subType = subType;
+    }
+
+
+}
